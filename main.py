25f0630a9397e725a9a57403209b290e5ff2c433
mevaristo <matheus.evaristo@protonmail.com>
2025-08-25 21:18:03 -0300

    :construction: define hat class init

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import copy
 import random
 
 class Hat:
-    pass
+    def __init__(self, **kwargs):
+        self.contents = [item for sublist in ([key for _ in range(value)] for key, value in kwargs.items()) for item in sublist]
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
+
+if __name__ == '__main__':
+    hat = Hat(red=4, green=2, blue=0)
